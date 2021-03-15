a1a02c3bc89a385104b11054cbb3e5c7ac66f6f6
Atef <mahmoudbenatef@gmail.com>
2021-03-16 00:03:22 +0200

    add_devise_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  def index
+    render "layouts/application"
+  end
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+  end
 end
