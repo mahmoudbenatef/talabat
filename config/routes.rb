43b728f76a22220d5073914b4d7be194aac607c1
Atef <mahmoudbenatef@gmail.com>
2021-03-19 02:59:12 +0200

    feature: add friend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "application#index"
+  root "friends#index"
+  resources :friends
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
