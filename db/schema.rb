6600814aa0e63f56e1440e4efa15c9a76f6a8019
Montaser <abdelrahmanmontaser12345@gmail.com>
2021-03-23 19:49:52 +0200

    third party facebook auth using omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_235015) do
-ActiveRecord::Schema.define(version: 2021_03_18_152411) do
+ActiveRecord::Schema.define(version: 2021_03_23_155526) do
 
   create_table "friends", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -22,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_18_152411) do
     t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
-  create_table "groups", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "groups", charset: "utf8mb4", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -34,7 +33,7 @@ ActiveRecord::Schema.define(version: 2021_03_18_152411) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "full_name", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -55,6 +54,8 @@ ActiveRecord::Schema.define(version: 2021_03_18_152411) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
